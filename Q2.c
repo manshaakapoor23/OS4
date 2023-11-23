85e7262aa1220518c00cbcb6d4c4d4558a313201
manshaakapoor23 <108448947+manshaakapoor23@users.noreply.github.com>
2023-11-24 00:25:10 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -4,105 +4,104 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define MAX_CARS 100
+#define MAX_PASSENGERS 100
 
-sem_t bridge_sema;     // Semaphore for controlling access to the bridge
-sem_t mutex;           // Mutex for critical sections
-int num_cars_on_bridge = 0;  // Number of cars currently on the bridge
+int total_passengers;
+int car_capacity;
 
-void passing(int dir, int car_id) {
-    if (dir == 0) {
-        printf("Left car %d is crossing the bridge.\n", car_id);
-    } else {
-        printf("Right car %d is crossing the bridge.\n", car_id);
-    }
-    sleep(1);  // Simulate the time it takes to cross the bridge
+sem_t car_sema;          // Semaphore for car
+sem_t passenger_sema;    // Semaphore for passengers
+sem_t mutex;             // Mutex for critical sections
+
+int passengers_on_board = 0;
+
+void load() {
+    printf("Car is loading passengers...\n");
+    sleep(2);
+    printf("Car is fully loaded and ready to go!\n");
 }
 
-void* left(void* args) {
-    int car_id = *((int*)args);
+void unload() {
+    printf("Car is unloading passengers...\n");
+    sleep(2);
+    printf("All passengers have disembarked.\n");
+}
 
+void board(int passenger_id) {
+    printf("Passenger %d is boarding.\n", passenger_id);
+    sleep(1);
+    printf("Passenger %d has boarded.\n", passenger_id);
+}
+
+void offboard(int passenger_id) {
+    printf("Passenger %d is getting off the car.\n", passenger_id);
+    sleep(1);
+    printf("Passenger %d has gotten off the car.\n", passenger_id);
+}
+
+void* car(void* args) {
     while (1) {
-        sem_wait(&mutex);
-        printf("Left car %d is waiting to cross.\n", car_id);
-        sem_post(&mutex);
-
-        sem_wait(&bridge_sema);
-
-        sem_wait(&mutex);
-        printf("Left car %d is on the bridge.\n", car_id);
-        num_cars_on_bridge++;
-        passing(0, car_id);
-        num_cars_on_bridge--;
-        printf("Left car %d has crossed the bridge.\n", car_id);
-        sem_post(&mutex);
-
-        sem_post(&bridge_sema);
-
-        sleep(1);  // Simulate some delay before the next car
+        sem_wait(&car_sema);  // Wait until passengers are ready to board
+        load();
+        sem_post(&passenger_sema);  // Signal passengers that car is loaded
+        sem_wait(&car_sema);  // Wait until all passengers have disembarked
+        unload();
     }
 }
 
-void* right(void* args) {
-    int car_id = *((int*)args);
+void* passenger(void* args) {
+    int passenger_id = *((int*)args);
 
-    while (1) {
-        sem_wait(&mutex);
-        printf("Right car %d is waiting to cross.\n", car_id);
-        sem_post(&mutex);
+    sem_wait(&mutex);
+    printf("Passenger %d is waiting to board.\n", passenger_id);
+    sem_post(&mutex);
 
-        sem_wait(&bridge_sema);
+    sem_post(&car_sema);  // Signal that passenger is ready to board
+    sem_wait(&passenger_sema);  // Wait for the car to be fully loaded
 
-        sem_wait(&mutex);
-        printf("Right car %d is on the bridge.\n", car_id);
-        num_cars_on_bridge++;
-        passing(1, car_id);
-        num_cars_on_bridge--;
-        printf("Right car %d has crossed the bridge.\n", car_id);
-        sem_post(&mutex);
+    sem_wait(&mutex);
+    board(passenger_id);
+    sem_post(&mutex);
 
-        sem_post(&bridge_sema);
+    sleep(3);  // Simulate ride duration
 
-        sleep(1);  // Simulate some delay before the next car
-    }
+    sem_wait(&mutex);
+    offboard(passenger_id);
+    sem_post(&mutex);
+
+    sem_post(&car_sema);  // Signal that passenger has disembarked
+
+    pthread_exit(NULL);
 }
 
 int main() {
-    int total_left_cars, total_right_cars;
+    printf("Enter the total number of passengers: ");
+    scanf("%d", &total_passengers);
 
-    printf("Enter the number of cars on the left: ");
-    scanf("%d", &total_left_cars);
+    printf("Enter the car capacity: ");
+    scanf("%d", &car_capacity);
 
-    printf("Enter the number of cars on the right: ");
-    scanf("%d", &total_right_cars);
-
-    if (total_left_cars <= 0 || total_right_cars <= 0 || total_left_cars + total_right_cars > MAX_CARS) {
+    if (car_capacity <= 0 || total_passengers <= 0 || car_capacity > MAX_PASSENGERS) {
         printf("Invalid input. Exiting.\n");
         return 1;
     }
 
-    sem_init(&bridge_sema, 0, 5);
+    sem_init(&car_sema, 0, 0);
+    sem_init(&passenger_sema, 0, 0);
     sem_init(&mutex, 0, 1);
 
-    pthread_t left_threads[total_left_cars], right_threads[total_right_cars];
-    int left_ids[total_left_cars], right_ids[total_right_cars];
+    pthread_t car_thread, passenger_threads[MAX_PASSENGERS];
+    int passenger_ids[MAX_PASSENGERS];
 
-    for (int i = 0; i < total_left_cars; ++i) {
-        left_ids[i] = i + 1;
-        pthread_create(&left_threads[i], NULL, left, (void*)&left_ids[i]);
+    pthread_create(&car_thread, NULL, car, NULL);
+
+    for (int i = 0; i < total_passengers; ++i) {
+        passenger_ids[i] = i + 1;
+        pthread_create(&passenger_threads[i], NULL, passenger, (void*)&passenger_ids[i]);
     }
 
-    for (int i = 0; i < total_right_cars; ++i) {
-        right_ids[i] = i + 1;
-        pthread_create(&right_threads[i], NULL, right, (void*)&right_ids[i]);
-    }
-
-    for (int i = 0; i < total_left_cars; ++i) {
-        pthread_join(left_threads[i], NULL);
-    }
-
-    for (int i = 0; i < total_right_cars; ++i) {
-        pthread_join(right_threads[i], NULL);
+    for (int i = 0; i < total_passengers; ++i) {
+        pthread_join(passenger_threads[i], NULL);
     }
 
     return 0;
